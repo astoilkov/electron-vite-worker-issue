e52297a9b91111e53298f6909ebdad0f76ad976d
Antonio Stoilkov <hello@astoilkov.com>
2022-10-04 17:39:24 +0300

    node import in web worker

--- packages/renderer/src/worker.ts
+++ packages/renderer/src/worker.ts
@@ -1,16 +1,3 @@
-// cjs
-// const electron = require('electron')
-// esm
-import electron from 'electron'
+import { join } from 'path'
 
-electron.clipboard
-electron.nativeImage
-electron.shell
-electron.contextBridge
-electron.crashReporter
-// 🐞 will cause the Renderer to crash
-// electron.ipcRenderer
-electron.webFrame
-
-// 🌱 works
-electron.clipboard.writeText('worker electron -> ' + Date.now())
+console.log(join('a', 'b'))
