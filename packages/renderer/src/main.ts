7a2df71da512ce0928d53aecbc02d893e6a8a205
Antonio Stoilkov <hello@astoilkov.com>
2022-10-08 19:01:07 +0300

    🐛 tildify throws an error

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,9 +1,13 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<p>hello</p>
-`
+import tildify from "tildify";
 
-document.body.addEventListener('click', () => {
-  new Worker(new URL('./worker.ts', import.meta.url), {
-    type: 'module'
-  })
-})
+console.log(tildify("/Users/astoilkov/awesome"));
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+<p>hello</p>
+`;
+
+document.body.addEventListener("click", () => {
+  new Worker(new URL("./worker.ts", import.meta.url), {
+    type: "module",
+  });
+});
