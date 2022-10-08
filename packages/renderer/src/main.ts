7ba388ec781f0bcf88de744a3951d1c0dd19cd92
Antonio Stoilkov <hello@astoilkov.com>
2022-10-08 19:07:34 +0300

    Revert "🐛 tildify throws an error"
    This reverts commit 7a2df71da512ce0928d53aecbc02d893e6a8a205.

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,13 +1,9 @@
-import tildify from "tildify";
-
-console.log(tildify("/Users/astoilkov/awesome"));
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <p>hello</p>
-`;
+`
 
-document.body.addEventListener("click", () => {
-  new Worker(new URL("./worker.ts", import.meta.url), {
-    type: "module",
-  });
-});
+document.body.addEventListener('click', () => {
+  new Worker(new URL('./worker.ts', import.meta.url), {
+    type: 'module'
+  })
+})
